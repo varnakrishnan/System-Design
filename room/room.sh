78fe31bb4397291cebc45cd258b6913898b46ca8
varnakrishnan <43096093+varnakrishnan@users.noreply.github.com>
2019-03-12 15:49:00 +0530

    Add files via upload

--- room/room.sh
+++ room/room.sh
@@ -14,13 +14,17 @@ do
 done < s1.csv
 #for i in ${rlno1[@]}; do echo $i; done
 #s2 count
+i=0
 while IFS="," read rno name
 do
   rlno2[$i]=$rno
   s2=`expr $s2 + 1`
   ((i++))
 done < s3.csv
+#for i in ${rlno2[@]}; do echo $i; done
+
 #s3 count
+i=0
 while IFS="," read rno name
 do
   rlno3[$i]=$rno
@@ -42,18 +46,93 @@ p=0
 q=0
 r=0
 f=0
+j=0
+f1=0
 while [ $i -le $tot_room ]
 do
-  echo 'Room ' $i
-	while [ $p -lt $s1 -a $q -lt $s2 -a $r -lt $s3 ]
+echo '==========================================='  
+echo 'Room ' $i
+	j=0
+	while [ $p -lt $s1 -a $j -lt 20 -a $q -lt $s2 -a $r -lt $s3 ]
 	do
    	if [ $f == 0 ]
 	then
-	#echo ${rlno2[$q]}
+	echo ${rlno1[$p]} ${rlno2[$q]}
 	((p++))
+	((q++))	
+	((j++))
+	f1=1
+	else
+	if [ $f == 1 ]
+	then
+	echo ${rlno2[$q]} ${rlno3[$r]}
 	((q++))
+	((r++))	
+	((j++))
+	f1=2
+	fi
+	fi
+	
+	if [ $f == 2 ]
+	then
+	echo ${rlno3[$r]} ${rlno1[$p]}
+	((r++))
+	((p++))	
+	((j++))
+	f1=0
 	fi
 	
 	done
-   i=`expr $i + 1`
+  i=`expr $i + 1`
+if [ $f1 == 1 ]
+then  
+f=1
+fi
+
+if [ $f1 == 2 ]
+then
+f=2
+fi
+
+if [ $f1 == 0 ]
+then
+f=0
+fi
+done
+
+while [ $p -lt $s1 -a $r -lt $s3 ]
+do
+	echo ${rlno3[$r]} ${rlno1[$p]}
+	((r++))
+	((p++))
+done
+while [ $q -lt $s2 -a $r -lt $s3 ]
+do
+	echo ${rlno3[$r]} ${rlno2[$q]}
+	((r++))
+	((q++))
+done
+while [ $q -lt $s2 -a $r -lt $s3 ]
+do
+	echo ${rlno1[$p]} ${rlno2[$q]}
+	((p++))
+	((q++))
+done
+
+while [ $p -lt $s1 ]
+do
+echo ${rlno1[$p]} 0
+((p++))
+done
+
+while [ $q -lt $s2 ]
+do
+echo ${rlno2[$q]} 0
+((q++))
+done
+
+while [ $r -lt $s3 ]
+do
+echo ${rlno3[$r]} 0
+((r++))
 done
